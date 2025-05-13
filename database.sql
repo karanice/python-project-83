16e27c29c16a035a051ea2b3ec75422b187f31ff
karanice <to.anathema@gmail.com>
2025-05-13 11:48:55 +0300

    Fix typos

--- database.sql
+++ database.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS urls (
     id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name VARCHAR(255) UNIQUE NOT NULL,
     created_at timestamp
-)
+);
 
 CREATE TABLE IF NOT EXISTS url_checks (
     id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
@@ -12,4 +12,4 @@ CREATE TABLE IF NOT EXISTS url_checks (
     title text, 
     description text,
     created_at timestamp
-)
+);
