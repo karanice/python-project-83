af7027777edcd559c24426917fe4642475c9dede
karanice <to.anathema@gmail.com>
2025-05-13 11:43:29 +0300

    Add SQL-script for creating tables in db

---
+++ database.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS urls (
+    id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(255) UNIQUE NOT NULL,
+    created_at timestamp
+)
+
+CREATE TABLE IF NOT EXISTS url_checks (
+    id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    url_id bigint REFERENCES urls(id) ON DELETE CASCADE,
+    status_code int, 
+    h1 text, 
+    title text, 
+    description text,
+    created_at timestamp
+)
