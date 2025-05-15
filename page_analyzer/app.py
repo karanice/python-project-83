7aec83e9e1ce23a508414bc675d238346b875ada
karanice <to.anathema@gmail.com>
2025-05-15 18:53:19 +0300

    Add logic for getting status code of response

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,6 +1,7 @@
 import os
-from urllib.parse import urlparse, urlunparse
+from urllib.parse import urlparse
 
+import requests
 from dotenv import load_dotenv
 from flask import (
     Flask,
@@ -25,12 +26,11 @@ url_repo = SiteRepository(app.config['DATABASE_URL'])
 check_repo = CheckRepository(app.config['DATABASE_URL'])
 
 
-def normalize_url(url):
-    parsed_url = urlparse(url)
-    normalized_url = parsed_url._replace(scheme=parsed_url.scheme.lower(),
-                                         netloc=parsed_url.netloc.lower(),
-                                         path=parsed_url.path.rstrip('/'))
-    return urlunparse(normalized_url)
+def normalize_root(url: str) -> str: 
+    parsed = urlparse(url)
+    scheme = parsed.scheme.lower()
+    netloc = parsed.netloc.lower()
+    return f"{scheme}://{netloc}"
 
 
 @app.route('/')
@@ -42,11 +42,12 @@ def index():
 @app.route('/urls', methods=['POST'])
 def urls_post():
     user_data = request.form.to_dict()
-    user_data['url'] = normalize_url(user_data['url'])
     urls = url_repo.get_content()
+
     urls_names = []
     for url in urls:
         urls_names.append(url['name'])
+
     is_valid = validate(user_data['url'])
     if is_valid is not True:
         flash('Некорректный URL', 'alert-danger')
@@ -56,14 +57,13 @@ def urls_post():
             messages=messages,
             url=user_data['url'],
         )
+    
+    user_data['url'] = normalize_root(user_data['url'])
     if user_data['url'] in urls_names:
         flash('Страница уже существует', 'alert-info')
-        messages = get_flashed_messages(with_categories=True)
-        return render_template(
-            'main.html',
-            messages=messages,
-            url=user_data['url'],
-        )
+        id = url_repo.find_by_name(user_data['url'])['id']
+        return redirect(url_for('urls_show', id=id), code=302)
+    
     if len(user_data['url']) > 255:
         flash('URL превышает 255 символов', 'alert-danger')
         messages = get_flashed_messages(with_categories=True)
@@ -85,7 +85,9 @@ def urls_get():
     for url in urls:
         url_with_last_check = url
         last_check = check_repo.get_last_check_date_by_id(url["id"])
+        last_status_code = check_repo.get_last_status_code_by_id(url["id"])
         url_with_last_check["last_check"] = last_check
+        url_with_last_check["status_code"] = last_status_code
         urls_with_last_check.append(url_with_last_check)
     return render_template(
         'urls.html',
@@ -96,10 +98,11 @@ def urls_get():
 @app.route('/urls/<id>')
 def urls_show(id):
     url = url_repo.find(id)
+    template = 'url.html' if url else 'incorrect_id.html'
     messages = get_flashed_messages(with_categories=True)
     checks = check_repo.get_content_by_url_id(id)
     return render_template(
-        'url.html',
+        template,
         url=url,
         messages=messages,
         checks=checks
@@ -108,12 +111,19 @@ def urls_show(id):
 
 @app.route('/urls/<id>/checks', methods=['POST'])
 def create_check(id):
-    check_repo.save(id)
+    url_row = url_repo.find(id)
+    url = url_row['name']
+    try:
+        r = requests.get(url, timeout=10)
+    except requests.exceptions.RequestException:
+        flash('Произошла ошибка при проверке', 'alert-danger')
+        return redirect(url_for('urls_show', id=id))
+    
+    status_code = r.status_code
+    if str(status_code)[0] in ('4','5'):
+        flash('Произошла ошибка при проверке', 'alert-danger')
+        return redirect(url_for('urls_show', id=id))
+    
+    check_repo.save(id, status_code)
     flash('Страница успешно проверена', 'alert-success')
     return redirect(url_for('urls_show', id=id))
-# render_template(
-#         'url.html',
-#         url=url,
-#         messages=messages,
-#         checks = checks
-#     )
