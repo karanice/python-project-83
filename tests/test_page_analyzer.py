fb4077b506aef22f9b5f780b29ce8d3a996c8446
karanice <to.anathema@gmail.com>
2025-05-21 13:24:32 +0300

    Add tests for main and index pages

--- tests/test_page_analyzer.py
+++ tests/test_page_analyzer.py
@@ -1,2 +1,15 @@
-def test_mock():
-    assert 1 == 1
+import pytest
+from page_analyzer.app import app
+
+@pytest.fixture 
+def client(): 
+    app.config['TESTING'] = True 
+    return app.test_client()
+
+def test_main_page(client): 
+    response = client.get('/') 
+    assert response.status_code == 200
+
+def test_urls_post_valid(client): 
+    response = client.post('/urls', data={'url':''})
+    assert response.status_code == 302
