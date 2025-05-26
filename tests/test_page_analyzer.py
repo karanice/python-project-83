6d9a056bed76fea483a38f16c1c4568c33f84f98
karanice <to.anathema@gmail.com>
2025-05-26 14:37:01 +0300

    Add tests

--- tests/test_page_analyzer.py
+++ tests/test_page_analyzer.py
@@ -1,15 +1,44 @@
-# import pytest
-# from page_analyzer.app import app
+import pytest
+import psycopg2
+import os
+from dotenv import load_dotenv
+from page_analyzer.app import app
 
-# @pytest.fixture 
-# def client(): 
-#     app.config['TESTING'] = True 
-#     return app.test_client()
+blobfish_url = 'https://ru.wikipedia.org/wiki/Psychrolutes_marcidus'
+invalid_url = 'kjdho72417ojef'
 
-# def test_main_page(client): 
-#     response = client.get('/') 
-#     assert response.status_code == 200
+load_dotenv()
 
-# def test_urls_post_valid(client): 
-#     response = client.post('/urls', data={'url':''})
-#     assert response.status_code == 302
+@pytest.fixture 
+def setup_db(): 
+    conn = psycopg2.connect(os.getenv('DATABASE_URL')) 
+    with conn.cursor() as cur: 
+        with open('database.sql', 'r') as f: 
+            cur.execute(f.read())
+        conn.commit()
+        yield 
+    with conn.cursor() as cur: 
+        cur.execute('TRUNCATE TABLE url_checks CASCADE') 
+        cur.execute('TRUNCATE TABLE urls CASCADE') 
+        conn.commit()
+        conn.close()
+
+@pytest.fixture 
+def client(): 
+    app.config['TESTING'] = True 
+    return app.test_client()
+
+def test_main_page(client): 
+    response = client.get('/') 
+    assert response.status_code == 200
+
+def test_urls_post_invalid(client, setup_db):
+    response = client.post('/urls', data={'url': invalid_url})
+    assert response.status_code == 422
+
+def test_urls_post_valid(client, setup_db): 
+    response = client.post('/urls', data={'url': blobfish_url})
+    assert response.status_code == 302
+
+
+
